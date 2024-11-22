1a0c91c19641ce5eb519e50eec5f6a26298f769a
G-SaiVishwas <gootysaivishwas12@gmail.com>
2024-11-22 16:22:31 +0530

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -6,11 +6,11 @@ class GameBoard {
     private char symbol1, symbol2;
     private int moves;
 
-    public final static int RESULT_1 = 1;
-    public final static int RESULT_2 = 2;
-    public final static int RESULT_3 = 3;
-    public final static int RESULT_4 = 4;
-    public final static int RESULT_5 = 5;
+    public final static int R1 = 1;
+    public final static int R2 = 2;
+    public final static int R3 = 3;
+    public final static int R4 = 4;
+    public final static int R5 = 5;
 
     public GameBoard(char symbol1, char symbol2) {
         grid = new char[size][size];
@@ -25,24 +25,24 @@ class GameBoard {
 
     public int action(char symbol, int a, int b) {
         if (a < 0 || a >= size || b < 0 || b >= size || grid[a][b] != ' ') {
-            return RESULT_5;
+            return R5;
         }
         grid[a][b] = symbol;
         moves++;
         if (grid[a][0] == grid[a][1] && grid[a][0] == grid[a][2]) {
-            return symbol == symbol1 ? RESULT_1 : RESULT_2;
+            return symbol == symbol1 ? R1 : R2;
         }
         if (grid[0][b] == grid[1][b] && grid[0][b] == grid[2][b]) {
-            return symbol == symbol1 ? RESULT_1 : RESULT_2;
+            return symbol == symbol1 ? R1 : R2;
         }
         if (grid[0][0] != ' ' && grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2]) {
-            return symbol == symbol1 ? RESULT_1 : RESULT_2;
+            return symbol == symbol1 ? R1 : R2;
         }
         if (grid[0][2] != ' ' && grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0]) {
-            return symbol == symbol1 ? RESULT_1 : RESULT_2;
+            return symbol == symbol1 ? R1 : R2;
         }
-        if (moves == size * size) return RESULT_3;
-        return RESULT_4;
+        if (moves == size * size) return R3;
+        return R4;
     }
 
     public void display() {
@@ -101,51 +101,51 @@ public class Tictactoe{
         p2 = playerDetails(2);
 
         while (p1.getSign() == p2.getSign()) {
-            System.out.println("Symbol already Taken !! Pick another symbol !!");
+            System.out.println("Open your eyes, symbol is already taken!");
             char sign = input.next().charAt(0);
             p2.setSign(sign);
         }
 
         board = new GameBoard(p1.getSign(), p2.getSign());
         boolean turn = true;
-        int result = GameBoard.RESULT_4;
+        int result = GameBoard.R4;
 
-        while (result == GameBoard.RESULT_4 || result == GameBoard.RESULT_5) {
+        while (result == GameBoard.R4 || result == GameBoard.R5) {
             if (turn) {
-                System.out.println("Player 1 - " + p1.getTitle() + "'s turn");
-                System.out.println("Enter x: ");
+                System.out.println("Player 1" + p1.getTitle() + "'s turn");
+                System.out.println("Enter move: ");
                 int x = input.nextInt();
-                System.out.println("Enter y: ");
+                System.out.println("Enter move: ");
                 int y = input.nextInt();
                 result = board.action(p1.getSign(), x, y);
-                if (result != GameBoard.RESULT_5) {
+                if (result != GameBoard.R5) {
                     turn = false;
                     board.display();
                 } else {
-                    System.out.println("Invalid Move !! Try Again !! ");
+                    System.out.println("Try Again, use a little of your brain!");
                 }
             } else {
-                System.out.println("Player 2 - " + p2.getTitle() + "'s turn");
-                System.out.println("Enter x: ");
+                System.out.println("Player 2" + p2.getTitle() + "'s turn");
+                System.out.println("Enter move: ");
                 int x = input.nextInt();
-                System.out.println("Enter y: ");
+                System.out.println("Enter move: ");
                 int y = input.nextInt();
                 result = board.action(p2.getSign(), x, y);
-                if (result != GameBoard.RESULT_5) {
+                if (result != GameBoard.R5) {
                     turn = true;
                     board.display();
                 } else {
-                    System.out.println("Invalid Move !! Try Again !! ");
+                    System.out.println("Dumdum make a proper move!");
                 }
             }
         }
 
-        if (result == GameBoard.RESULT_1) {
-            System.out.println("Player 1 - " + p1.getTitle() + " wins !!");
-        } else if (result == GameBoard.RESULT_2) {
-            System.out.println("Player 2 - " + p2.getTitle() + " wins !!");
+        if (result == GameBoard.R1) {
+            System.out.println(p1.getTitle() + " won");
+        } else if (result == GameBoard.R2) {
+            System.out.println(p2.getTitle() + " won");
         } else {
-            System.out.println(" !! DRAW !!");
+            System.out.println("Both of you are useless");
         }
     }
 
